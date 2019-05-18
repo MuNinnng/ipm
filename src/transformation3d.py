fix X rotation error

src/transformation3d.py
@@ -64,15 +64,12 @@ def rotate_x(p, a=0):
     # turn value to radians
     a = math.radians(a)
     translation_mat = np.matrix([
-        [math.cos(a),math.sin(a),0,0],
-        [-math.sin(a),math.cos(a),0,0],
-        [0,0,0,0],
+        [1,0,0,0],
+        [0,math.cos(a),math.sin(a),0],
+        [0,-math.sin(a),math.cos(a),0],
         [0,0,0,1],
         ], dtype="float32")
 
     new_p = p @ translation_mat
 
     return new_p
-
-
-    