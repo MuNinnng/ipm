8cc29b3fe25eb9101555291ded6ef0d9023ea2e6
Roman Peresoliak <roman.peresoliak@gmail.com>
2019-05-20 17:15:48 +0300

    update matrix class definition

--- src/matrix.py
+++ src/matrix.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
+import math
+
+import numpy as np
+
+
+def to_3d(coord):
+    w = coord[:,3]
+    res = np.zeros(coord.shape)
+    print(w)
+    res[:,0] = coord[:,0]/w
+    res[:,1] = coord[:,1]/w
+    res[:,2] = coord[:,2]/w
+    return res[:,:3]
 
 class Matrix(object):
+    def __init__(self):
+        pass
 
     def perspective(self, fov, aspect, near, far):
         """Generates a perspective projection matrix with given bounds.
@@ -13,4 +28,57 @@ class Matrix(object):
         :return: new 4x4 np.matrix
         """
 
-        pass
+        dist = far - near
+
+        a = (far+near)/dist
+        # b = (2*near*far)/dist
+        b = (2*near*far)/(near - far)
+
+        alpha = math.radians(fov/2)
+        # print(alpha)
+        # calc contagent
+        cot = 1. / math.tan(alpha)
+
+        perspective_mat = np.array([
+            [cot,0,0,0],
+            [0,cot,0,0],
+            [0,0,a,1],
+            [0,0,b,1]
+            ], dtype="float32")
+
+        return perspective_mat
+
+
+if __name__ == "__main__":
+    from matplotlib import pyplot as plt
+
+    m = Matrix()
+    p_mat = m.perspective(45, 1, 20, 5000)
+
+    p = np.array([[0,0,20, 1]])
+    p_new = p @ p_mat
+    p_new = np.floor(p_new)
+    print(p_new)
+    pp = to_3d(p_new)
+    print(pp)
+
+
+
+
+    square = np.array([
+    [-1,-1,1,1],
+    [-1,1,1,1],
+    [1,1,1,1],
+    [1,-1,1,1],
+    [-1,-1,1,1],
+    ])
+
+    p_square = square @ p_mat
+    p_square3d = to_3d(p_square)
+    print(p_square)
+    print(square[:,0])
+    print(p_square[:,0])
+
+    # plt.scatter(square[:,0], square[:,1])
+    plt.scatter(p_square3d[:,0], p_square3d[:,1])
+    plt.show()
