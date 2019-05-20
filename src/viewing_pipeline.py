c7ab641c4fee3e9b6c1d853e665ba80c2bb8efb6
Roman Peresoliak <roman.peresoliak@gmail.com>
2019-05-20 17:41:23 +0300

    integrate transformation funcs to pipeline

--- src/viewing_pipeline.py
+++ src/viewing_pipeline.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
+from matrix import Transform
 
 
 class Pipeline(object):
+    def __init__(self):
+        transform = Transform()
+
+    def camera_transformation(self, points):
+        cam_matrix = self.transform.get_camera_matrix()
+        return points @ cam_matrix
+
     def to_image_space(self):
         pass
 
     def project(self, points):
         # convert objects to camera view
-        res = camera_transform()
-        # convert to perspective view
-        res = to_image_space(res)
-        # convert to screen view(projection)
-        res = to_screen_space(res)
+        res = camera_transform(points)
+        # # convert to perspective view
+        # res = to_image_space(res)
+        # # convert to screen view(projection)
+        # res = to_screen_space(res)
