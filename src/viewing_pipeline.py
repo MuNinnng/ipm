1b713d10d42df4062d3f30a9d0c2a73428734594
Roman Peresoliak <roman.peresoliak@gmail.com>
2019-05-20 19:54:54 +0300

    turn pixel to NDC points

--- src/viewing_pipeline.py
+++ src/viewing_pipeline.py
@@ -26,18 +26,39 @@ class Pipeline(object):
         # # convert to screen view(projection)
         # res = to_screen_space(res)
 
-    def upproject(self, pixels):
-        w,h = self.viewport.size
-        # the Normalized Device Coordinates (NDC).
-        # turn pixel value to Image space coordinates
-        ndc_coords = np.zeros((pixels.shape[0], 4))
-        ndc_coords[:,0] = (ndc_coords[:,0] / w * 2) - 1
-        ndc_coords[:,1] = (ndc_coords[:,1] / h * 2) - 1
-        ndc_coords[:,2] = 0
+    def pixel_to_ndc(self, pixels, viewport_size):
+        """Convert pixel values to Normalized Device Coordinates."""
+        w,h = viewport_size
+
+        ndc_coords = np.zeros((pixels.shape[0], 4), dtype="float32")
+        ndc_coords[:,0] = (pixels[:,0] / w * 2) - 1
+        ndc_coords[:,1] = (pixels[:,1] / h * 2) - 1
+        ndc_coords[:,2] = -1
         ndc_coords[:,3] = 1
 
+        return ndc_coords
 
-        pass
+    def unproject(self, pixels, viewport_size):
+        # the Normalized Device Coordinates (NDC).
+        # turn pixel value to Image space coordinates
+        ndc_near_coords = self.pixel_to_ndc(pixels, viewport_size)
+        ndc_far_coords = np.copy(ndc_near_coords)
+        # set pixel point at far plane
+        ndc_far_coords[:, 2] = 1
+
+        perspective_mat = self.transform.perspective_matrix
+        view_mat = self.transform.get_camera_matrix()
+        # FIXME does not work for some reason
+        unproject_mat = np.linalg.inv(perspective_mat @ view_mat)
+
+        new_near_points = ndc_near_coords @ unproject_mat
+        new_near_points = self.transform.homogenus_to_world(new_near_points)
+
+        new_far_points = ndc_far_coords @ unproject_mat
+        new_far_points = self.transform.homogenus_to_world(new_far_points)
+
+        print(new_near_points)
+        print(new_far_points)
 
 
 if __name__ == "__main__":
@@ -63,14 +84,21 @@ if __name__ == "__main__":
         [1,-1,0,1],
         ])
 
+    pixels = np.array([
+        [25,25]
+        ])
+
+
     pl = Pipeline()
     pl.transform.set_camera([0,0,50])
-    projected_p = pl.project(square)
+    # projected_p = pl.project(square)
 
-    vp = Viewport((100,200))
-    vp.render(projected_p)
+    unprojected = pl.unproject(pixels, (50,50))
 
-    print(vp.data)
+    # vp = Viewport((100,200))
+    # vp.render(projected_p)
 
-    plt.imshow(vp.data)
-    plt.show()
+    # print(vp.data)
+
+    # plt.imshow(vp.data)
+    # plt.show()
